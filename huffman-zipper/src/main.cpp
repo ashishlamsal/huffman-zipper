d4fd48a2030c5482d0d54f7d129a0edb0dde53a6
ashish <ashishlamsal4283@gmail.com>
2020-11-12 18:33:23 +0545

    Nothing Changed

--- huffman-zipper/src/main.cpp
+++ huffman-zipper/src/main.cpp
@@ -1,34 +1,5 @@
-#include <iostream>
-
-
 #include "Compressor.h"
 #include "Decompressor.h"
-
-
-
-
-
-
-
-
-std::string decodeCharacters(BinNode* root, std::string encodedString) {
-	std::string decodedString = "";
-	BinNode* curr = root;
-	for (int i = 0; i < encodedString.size(); i++) {
-		if (encodedString[i] == '0')
-			curr = curr->getLeftChild();
-		else
-			curr = curr->getRightChild();
-
-		// reached leaf node 
-		if (curr->getLeftChild() == nullptr && curr->getRightChild() == nullptr) {
-			decodedString += curr->getCharacter();
-			curr = root;
-		}
-	}
-	return decodedString;
-}
-
 
 
 int main() {
@@ -39,20 +10,10 @@ int main() {
 		compression.compressor(INPUT_FILE_PATH);
 	}*/
 
-
 	//-----------------------Decompression-------------------------//
 	Decompressor decompression;
 
 	decompression.decompressor(COMPRESSED_FILE_PATH);
-	// 2. Read encodedString from file
-
-	
-	// 3. Build decoding tree
-
-	// 4. decode string
-	// 5. write decoded string to file
-
-	
 
 	return 0;
 };
