020b84ce54ea8dbe16baf97c243cbb6babeb62ab
ashish <ashishlamsal4283@gmail.com>
2020-12-05 15:17:47 +0545

    GUI

--- huffmanGUI/src/noname.cpp
+++ huffmanGUI/src/noname.cpp
@@ -62,6 +62,7 @@ void MyFrame1::OnCompressClick(wxCommandEvent& event) {
 	}
 	wxString str = this->m_textCtrl1->GetValue();
 }
+
 void MyFrame1::OnChooseClick(wxCommandEvent& event) {
 	tooglePicker();
 }
@@ -85,11 +86,11 @@ void MyFrame1::compress(std::string input) {
 	if (!selection)
 		compression.compressFile(input);
 	else
-		compression.compressFolder(input); 
+		compression.compressFolder(input);
 }
 
 void MyFrame1::decompress(std::string input) {
-		decompression.decompressFile(input);
+	decompression.decompressFile(input);
 }
 
 
@@ -112,87 +113,85 @@ void MyFrame1::tooglePicker() {
 
 }
 
-MyFrame1::MyFrame1( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxFrame( parent, id, title, pos, size, style )
+MyFrame1::MyFrame1(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : wxFrame(parent, id, title, pos, size, style)
 {
-	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
-	
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
+
 	wxBoxSizer* bSizer1;
-	bSizer1 = new wxBoxSizer( wxVERTICAL );
-	
+	bSizer1 = new wxBoxSizer(wxVERTICAL);
+
 	wxString m_radioBox3Choices[] = { wxT("File"), wxT("Folder") };
-	int m_radioBox3NChoices = sizeof( m_radioBox3Choices ) / sizeof( wxString );
-	m_radioBox3 = new wxRadioBox( this, wxID_ANY, wxT("Choose"), wxDefaultPosition, wxDefaultSize, m_radioBox3NChoices, m_radioBox3Choices, 1, wxRA_SPECIFY_ROWS );
-	m_radioBox3->SetSelection( 1 );
-	bSizer1->Add( m_radioBox3, 0, wxALL, 5 );
-	
+	int m_radioBox3NChoices = sizeof(m_radioBox3Choices) / sizeof(wxString);
+	m_radioBox3 = new wxRadioBox(this, wxID_ANY, wxT("Choose"), wxDefaultPosition, wxDefaultSize, m_radioBox3NChoices, m_radioBox3Choices, 1, wxRA_SPECIFY_ROWS);
+	m_radioBox3->SetSelection(1);
+	bSizer1->Add(m_radioBox3, 0, wxALL, 5);
+
 
 	m_filePicker1 = new wxFilePickerCtrl(this, wxID_ANY, wxEmptyString, wxT("Select a file"), wxT("*.*"), wxDefaultPosition, wxDefaultSize, wxFLP_DEFAULT_STYLE);
 	bSizer1->Add(m_filePicker1, 0, wxALL | wxEXPAND, 5);
-	
+
 	m_dirPicker1 = new wxDirPickerCtrl(this, wxID_ANY, wxEmptyString, wxT("Select a folder"), wxDefaultPosition, wxDefaultSize, wxDIRP_DEFAULT_STYLE);
 	bSizer1->Add(m_dirPicker1, 0, wxALL | wxEXPAND, 5);
 
 	tooglePicker();
-	
+
 	wxBoxSizer* bSizer2;
-	bSizer2 = new wxBoxSizer( wxHORIZONTAL );
-	
-	m_button1 = new wxButton( this, ID_CompressButtonClick, wxT("Compress"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer2->Add( m_button1, 1, wxALL, 5 );
-	
-	m_button2 = new wxButton( this, wxID_ANY, wxT("Decompress"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer2->Add( m_button2, 1, wxALL, 5 );
-	
-	
-	bSizer1->Add( bSizer2, 0, wxALIGN_CENTER_HORIZONTAL|wxEXPAND, 5 );
-	
+	bSizer2 = new wxBoxSizer(wxHORIZONTAL);
+
+	m_button1 = new wxButton(this, ID_CompressButtonClick, wxT("Compress"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer2->Add(m_button1, 1, wxALL, 5);
+
+	m_button2 = new wxButton(this, wxID_ANY, wxT("Decompress"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer2->Add(m_button2, 1, wxALL, 5);
+
+
+	bSizer1->Add(bSizer2, 0, wxALIGN_CENTER_HORIZONTAL | wxEXPAND, 5);
+
 	wxBoxSizer* bSizer3;
-	bSizer3 = new wxBoxSizer( wxHORIZONTAL );
-	
-	m_textCtrl1 = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer3->Add( m_textCtrl1, 3, wxALL|wxEXPAND, 5 );
-	
+	bSizer3 = new wxBoxSizer(wxHORIZONTAL);
+
+	m_textCtrl1 = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0);
+	bSizer3->Add(m_textCtrl1, 3, wxALL | wxEXPAND, 5);
+
 	wxBoxSizer* bSizer4;
-	bSizer4 = new wxBoxSizer( wxVERTICAL );
-	
-	m_button3 = new wxButton( this, wxID_ANY, wxT("Clear"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer4->Add( m_button3, 0, wxALL, 5 );
-	
-	
-	bSizer4->Add( 0, 0, 1, wxEXPAND, 5 );
-	
-	m_button4 = new wxButton( this, wxID_ANY, wxT("Help"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer4->Add( m_button4, 0, wxALL, 5 );
-	
-	m_button5 = new wxButton( this, wxID_ANY, wxT("About"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer4->Add( m_button5, 0, wxALL, 5 );
-	
-	m_button6 = new wxButton( this, wxID_ANY, wxT("Quit"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer4->Add( m_button6, 0, wxALL, 5 );
-	
-	
-	bSizer3->Add( bSizer4, 0, wxEXPAND, 5 );
-	
-	
-	bSizer1->Add( bSizer3, 1, wxEXPAND, 5 );
-	
-	
-	this->SetSizer( bSizer1 );
+	bSizer4 = new wxBoxSizer(wxVERTICAL);
+
+	m_button3 = new wxButton(this, wxID_ANY, wxT("Clear"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer4->Add(m_button3, 0, wxALL, 5);
+
+
+	bSizer4->Add(0, 0, 1, wxEXPAND, 5);
+
+	m_button4 = new wxButton(this, wxID_ANY, wxT("Help"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer4->Add(m_button4, 0, wxALL, 5);
+
+	m_button5 = new wxButton(this, wxID_ANY, wxT("About"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer4->Add(m_button5, 0, wxALL, 5);
+
+	m_button6 = new wxButton(this, wxID_ANY, wxT("Quit"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer4->Add(m_button6, 0, wxALL, 5);
+
+
+	bSizer3->Add(bSizer4, 0, wxEXPAND, 5);
+
+
+	bSizer1->Add(bSizer3, 1, wxEXPAND, 5);
+
+
+	this->SetSizer(bSizer1);
 	this->Layout();
-	
-	this->Centre( wxBOTH );
-	
+
+	this->Centre(wxBOTH);
+
 	// Connect Events
-	m_radioBox3->Connect( wxEVT_COMMAND_RADIOBOX_SELECTED, wxCommandEventHandler( MyFrame1::OnChooseClick ), NULL, this );
-	m_button1->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::OnCompressClick ), NULL, this );
-	m_button2->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::OnDecompressClick ), NULL, this );
+	m_radioBox3->Connect(wxEVT_COMMAND_RADIOBOX_SELECTED, wxCommandEventHandler(MyFrame1::OnChooseClick), NULL, this);
+	m_button1->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::OnCompressClick), NULL, this);
+	m_button2->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::OnDecompressClick), NULL, this);
 }
 
-MyFrame1::~MyFrame1()
-{
+MyFrame1::~MyFrame1() {
 	// Disconnect Events
-	m_radioBox3->Disconnect( wxEVT_COMMAND_RADIOBOX_SELECTED, wxCommandEventHandler( MyFrame1::OnChooseClick ), NULL, this );
-	m_button1->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::OnCompressClick ), NULL, this );
-	m_button2->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyFrame1::OnDecompressClick ), NULL, this );
-	
+	m_radioBox3->Disconnect(wxEVT_COMMAND_RADIOBOX_SELECTED, wxCommandEventHandler(MyFrame1::OnChooseClick), NULL, this);
+	m_button1->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::OnCompressClick), NULL, this);
+	m_button2->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::OnDecompressClick), NULL, this);
 }
--- huffmanGUI/src/noname.h
+++ huffmanGUI/src/noname.h
@@ -26,41 +26,41 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// Class MyFrame1
 ///////////////////////////////////////////////////////////////////////////////
-class MyFrame1 : public wxFrame 
+class MyFrame1 : public wxFrame
 {
-	private:
-	
-		void OnChooseClick(wxCommandEvent& event); //{ event.Skip(); }
-		void OnCompressClick(wxCommandEvent& event); //{ event.Skip(); }
-		void OnDecompressClick(wxCommandEvent& event); //{ event.Skip(); }
-		wxDECLARE_EVENT_TABLE();
-		Compressor compression;
-		Decompressor decompression;
-		int selection;
-		void tooglePicker();
-		void compress(std::string );
-		void decompress(std::string );
+private:
 
-	protected:
-		wxRadioBox* m_radioBox3;
-		wxFilePickerCtrl* m_filePicker1;
-		wxDirPickerCtrl* m_dirPicker1;
-		wxButton* m_button1;
-		wxButton* m_button2;
-		wxTextCtrl* m_textCtrl1;
-		wxButton* m_button3;
-		wxButton* m_button4;
-		wxButton* m_button5;
-		wxButton* m_button6;
-		
-		// Virtual event handlers, overide them in your derived class
-		
-	
-	public:
-		
-		MyFrame1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,448 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
-		
-		~MyFrame1();
-	
+	void OnChooseClick(wxCommandEvent& event); //{ event.Skip(); }
+	void OnCompressClick(wxCommandEvent& event); //{ event.Skip(); }
+	void OnDecompressClick(wxCommandEvent& event); //{ event.Skip(); }
+	wxDECLARE_EVENT_TABLE();
+	Compressor compression;
+	Decompressor decompression;
+	int selection;
+	void tooglePicker();
+	void compress(std::string);
+	void decompress(std::string);
+
+protected:
+	wxRadioBox* m_radioBox3;
+	wxFilePickerCtrl* m_filePicker1;
+	wxDirPickerCtrl* m_dirPicker1;
+	wxButton* m_button1;
+	wxButton* m_button2;
+	wxTextCtrl* m_textCtrl1;
+	wxButton* m_button3;
+	wxButton* m_button4;
+	wxButton* m_button5;
+	wxButton* m_button6;
+
+	// Virtual event handlers, overide them in your derived class
+
+
+public:
+
+	MyFrame1(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(500, 448), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+
+	~MyFrame1();
+
 };
 
